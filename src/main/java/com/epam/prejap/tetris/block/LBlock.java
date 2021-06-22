7bab1f5e2c122558470776286d702bf88d042029
Dzmitry Tatarski <dzmitry_tatarski@epam.com>
2021-06-22 14:21:40 +0200

    remove @version in LBlock

--- src/main/java/com/epam/prejap/tetris/block/LBlock.java
+++ src/main/java/com/epam/prejap/tetris/block/LBlock.java
@@ -4,7 +4,6 @@ package com.epam.prejap.tetris.block;
  * Tetris game, in a byte array, generates a block "L", where "1" in byte array is printed as "#".
  * @see BlockFeed
  * @author Dzmitry_Tatarski
- * @version 1.0
  */
 final class LBlock extends Block {
 
